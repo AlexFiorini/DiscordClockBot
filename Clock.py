b8e2a1a8c2fedc66e1137ef722a958262a200714
Alex Fiorini <61363883+AlexFiorini@users.noreply.github.com>
2022-08-26 16:26:20 +0200

    Add files via upload

---
+++ Clock.py
@@ -0,0 +1,41 @@
+# bot.py
+from email import message
+import os
+
+from datetime import datetime
+from time import sleep
+import pytz
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("DISCORD_GUILD")
+
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+    channel = client.get_channel(1012711976046182501)
+
+    tz_Rome = pytz.timezone('Europe/Rome')
+    datetime_Rome = datetime.now(tz_Rome)
+    message = await channel.fetch_message(1012726198352228484)
+
+    while 1:
+        if datetime_Rome != datetime.now(tz_Rome):
+            datetime_Rome = datetime.now(tz_Rome)
+            testo=datetime_Rome.strftime("%H:%M")
+            await message.edit(content=testo)
+
+client.run(TOKEN)
